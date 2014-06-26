21509998350a42e89a856e914bd94c5b4e0b01c3
Chibuzor Alumba <coolchibs@gmail.com>
2014-06-25 19:14:48 -0500

    So far I have created an ember app without a controller defined, created a model inline(html) called name, put that inside an input type=text tab and have it update elsewhere when I change the text in the input tag. Now I have defined my own controller, created a model and bound a function to that model. I have also called the function from html

--- js/app.js
+++ js/app.js
@@ -1,81 +1,13 @@
-App = Ember.Application.create(
-	//{
-		//socket: window.io.connect('http://localhost');
-		/*socket.on('connection', function(data){                                                                               
-      		//self.store.push(data.type, data.item);
-      		console.log('Socket connected');                                                                             
-    	});*/
-	//}
-	);
+App = Ember.Application.create();
 
-App.ApplicationRoute = Ember.Route.extend({                                                              
-  activate: function() {
-  	console.log('Ember active is called');               
-    // connect to the websocket once we enter the application route                                                                   
-    var socket = window.io.connect('http://localhost');                                                           
+App.ApplicationController = Ember.Controller.extend({
+	name: '',
+	shout: function() {
+		return 'Hello ' + this.get('name').toUpperCase() + '!';
+	}.property('name'),
 
-    var self = this;                                                                                                   
+	whisper: function(){
 
-    socket.on('message', function(data){                                                                               
-      //self.store.push(data.type, data.item);                                                                              
-    });
-    socket.on('connection', function(data){                                                                               
-      //self.store.push(data.type, data.item);
-      console.log('Socket connected');                                                                             
-    });                                                                                                                   
-  }
+		return 'heyyyy ' + this.get('name').toLowerCase() + '...';
+	}.property('name')
 });
-
-App.Router.map(function(){
-	this.resource('about');
-	this.resource('posts', function(){
-		this.resource('post', { path: ':post_id'});
-	});
-	//this.resource('app');
-});
-
-App.PostsRoute = Ember.Route.extend({
-	model: function(){
-		return posts;
-	}
-});
-
-App.PostRoute = Ember.Route.extend({
-	model: function(params){
-		return posts.findBy('id', params.post_id);
-	}
-});
-
-App.PostController = Ember.ObjectController.extend({
-	isEditing: false,
-
-	actions: {
-		edit: function(){
-			this.set('isEditing', true);
-		},
-
-		doneEditing: function() {
-			this.set('isEditing', false);
-		}
-	}
-});
-
-Ember.Handlebars.helper('format-date', function(date){
-	return moment(date).fromNow();
-});
-
-var posts = [{
-	id: '1',
-	title: "Rails is Good",
-	author: {name: "sam"},
-	date: new Date('12-27-2014'),
-	excerpt: "Lorem ipsums",
-	body: "Some template info to use"
-}, {
-	id: '2',
-	title: "Rails is bad",
-	author: {name: "sam"},
-	date: new Date('12-27-2014'),
-	excerpt: "Lorem ipsumssss",
-	body: "Some other template info to be used"
-}];
